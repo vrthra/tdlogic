08c0d91892eb85d4372b7e6d003a5878d4d6a624
blufox@gmail.com <blufox@gmail.com@7f9c0d80-2fbc-8f51-948a-28202112dc5a>
2012-04-04 03:02:39 +0000

    Fix .todo.

--- trunk/.todo/db.pl
+++ trunk/.todo/db.pl
@@ -1,31 +1,32 @@
+db(academics,'Things to do with academics').
 db(admission,'Things to do with admission').
-db(life,'Things to do with life').
-db(dl,'get driving license').
 db(apartment,'get apartment for spring').
 db(books,'books to read').
 db(capek,'war with the newts').
+db(conra,'CONRA_2011').
+db(cs321,'Theory of Computation').
+db(cs507,'Grad Seminar').
+db(cs533,'Intelligent Agents').
+db(cs581,'Programming Languages').
+db(cs582,'Object Oriented Analysis and Design').
+db(czeckmovies,'Czeck Movies').
+db(dl,'get driving license').
 db(dobry,'Dobry vojak Svejk').
-db(movies,'Movies to watch').
-db(czeck_movies,'Czeck Movies').
-db(kuky,'Kuky se vraci').
+db(ihouse,'iHouse').
+db(intern,'get internship').
 db(kolja,'Kolja - Jan Sverak').
-db(wild,'Wild Movie').
-db(tax,'Tax').
-db(academics,'Things to do with academics').
+db(kuky,'Kuky se vraci').
+db(life,'Things to do with life').
+db(main,'Main').
+db(meeting,'Meet with Dr Budd').
+db(movies,'Movies to watch').
+db(paper,'Papeer for VLHCC').
 db(prilim,'Decide prilim topic').
 db(quals,'PhD quals').
-db(cs582,'Object Oriented Analysis and Design').
-db(cs321,'Theory of Computation').
-db(cs581,'Programming Languages').
-db(cs533,'Intelligent Agents').
-db(cs507,'Grad Seminar').
+db(rent,'Pay 1033 to steve').
 db(research,'Research Topics').
 db(ta,'cs321 t.a').
 db(ta,'cs480 t.a').
-db(intern,'get internship').
-db(ihouse,'iHouse').
-db(rent,'Pay 1033 to steve').
-db(paper,'Papeer for VLHCC').
-db(conra,'CONRA_2011').
-db(meeting,'Meet with Dr Budd').
+db(tax,'Tax').
+db(wild,'Wild Movie').
 db(spring:2012,'Spring Term').
